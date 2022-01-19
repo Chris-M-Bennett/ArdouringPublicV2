81113789c43b47b6410d632a508aac89bf49b72a
Chris-M-Bennett <cmbennett1@hotmail.com>
2022-01-19 17:12:53 +0000

    Fixin Tings

---
+++ Assets/Scripts/System/SceneLoadScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace System
+{
+   public class SceneLoadScript : MonoBehaviour
+   {
+      [SerializeField] SceneAsset sceneToLoad;
+      private void OnTriggerEnter2D(Collider2D player)
+      {
+         if (player.CompareTag("Player"))
+         {
+            SceneManager.LoadSceneAsync(sceneToLoad.name);
+         }
+      }
+   }
+}
