a2873d0ad68eb2aa118821be47023b88be5b63a7
Chris-M-Bennett <cmbennett1@hotmail.com>
2022-01-31 13:59:07 +0000

    Pause

---
+++ Assets/Scripts/UI/PauseScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+
+    public class PauseScript : MenuScript
+    {
+        [SerializeField] private Toggle tutToggle;
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+            tutToggle.isOn = Convert.ToBoolean(PlayerPrefs.GetInt("Tutorials", 1));
+        }
+
+        public void MainMenu(){
+            SceneManager.LoadSceneAsync("MainMenu");
+            
+        }
+    }
+}
