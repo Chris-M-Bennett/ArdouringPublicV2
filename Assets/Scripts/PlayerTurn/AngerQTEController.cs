ee642b66e87a69b073499e957a5e1a005876c54c
NewTides <jlevy.75583@farnborough.ac.uk>
2022-03-10 19:58:19 +0000

    Added assets for player turn QTEs and functioning QTEs for Anger & Pride (currently not hooked up)

---
+++ Assets/Scripts/PlayerTurn/AngerQTEController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngerQTEController : MonoBehaviour
+{
+    private float timeLimit, timer, markerY, upForce, dropSpeed;
+    public float multiplierA;
+    private AngerQTEController _marker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLimit = 5f;
+        timer = 0f;
+        upForce = 20f;
+        dropSpeed = 1f;
+        _marker = GetComponent<AngerQTEController>();
+        // marker start y = -1.65f, marker x = -0.25
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timer >= timeLimit)
+        {
+            multiplierA = (_marker.transform.position.y / 3.3f) + 1f;
+            //Debug.Log("Anger damage multiplier: " + multiplierA);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            _marker.transform.Translate(0f, -1f * dropSpeed * Time.deltaTime, 0f, Space.World);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _marker.transform.Translate(0f, 1f * upForce * Time.deltaTime, 0f, Space.World);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerTurn/PrideQTEController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrideQTEController : MonoBehaviour
+{
+    private float distFromCrit, maxDist, timer, timeLimit, multiplierP;
+    private bool stop;
+    private PrideQTEController _marker;
+    public GameObject CritPoint;
+    public GameObject PrideRing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLimit = 5f;
+        timer = 0f;
+        maxDist = 1.4f;
+        stop = false;
+        _marker = GetComponent<PrideQTEController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= timeLimit || Input.GetKeyDown(KeyCode.E) || stop)
+        {
+            stop = true;
+            distFromCrit = Vector2.Distance (_marker.transform.position, CritPoint.transform.position);
+            multiplierP = (((distFromCrit - maxDist) * -1) / maxDist) + 0.5f;
+            //Debug.Log("Pride damage multiplier: " + multiplierP);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            transform.RotateAround(PrideRing.transform.position, Vector3.forward, 144 * Time.deltaTime);
+            
+        }
+    }
+}
