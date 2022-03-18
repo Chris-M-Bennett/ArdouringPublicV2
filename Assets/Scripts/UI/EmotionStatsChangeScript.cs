a7a9178ae8cad6b0b1cb09040f1cba2963d24499
Chris-M-Bennett <cmbennett1@hotmail.com>
2022-03-18 16:23:26 +0000

    Conversation System Implemented

---
+++ Assets/Scripts/UI/EmotionStatsChangeScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class EmotionStatsChangeScript : MonoBehaviour
+    {
+        [SerializeField] private Text emotStat;
+        [SerializeField] private string emotName;
+        [SerializeField] private bool add;
+        private int changeAmount;
+        private int current;
+
+        private void Start()
+        {
+            current = PlayerPrefs.GetInt(emotName, 1);
+            emotStat.text = current.ToString();
+        }
+
+        public void ChangeEmotAmount()
+        {
+            if (add && changeAmount < 3)
+            {
+                changeAmount++;
+            }
+            else if (!add && changeAmount >= 0)
+            {
+                changeAmount--;
+            }
+            emotStat.text = $"{current+changeAmount}";
+        }
+
+    }
+}
